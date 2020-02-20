eb30d2162661d83eea3fdf03de12cae0765ee682
hamidreza nasrollahy <h.mosaferkocholo@gmail.com>
2020-02-20 21:27:01 +0330

    done with promises

--- promise/promise.js
+++ promise/promise.js
@@ -139,3 +139,114 @@ then_able_promise
         //  onFinally
         console.log('...finally')
     })
+
+/* -------------------------------------------------------------------------- */
+/*                     promise resolve and promise reject                     */
+/* -------------------------------------------------------------------------- */
+
+let shortcut_promise = (function() {
+    if (Math.random() < 0.5) {
+        return Promise.resolve('resolve promise')
+    } else {
+        return Promise.reject('reject promise')
+    }
+})()
+shortcut_promise
+    .then(val => {
+        // onFulfilled
+        console.log(val)
+    })
+    .catch(error => {
+        // onRejected
+        console.log(error)
+    })
+    .finally(() => {
+        //  onFinally
+        console.log('...finally')
+    })
+
+let shortcut_promise2 = (function() {
+    if (Math.random() < 0.5) {
+        return Promise.resolve('resolve promise')
+    } else {
+        return Promise.reject('reject promise')
+    }
+})()
+shortcut_promise2
+    .then(val => {
+        // onFulfilled
+        console.log(val)
+        return Promise.resolve('2nd fulfilled promise')
+    })
+    .then(val => {
+        console.log(val)
+    })
+    .catch(error => {
+        // onRejected
+        console.log('error from 186 line: ', error)
+    })
+    .finally(() => {
+        //  onFinally
+        console.log('...finally')
+    })
+
+// you cannot do this for async functions
+// function setTime() {
+//     setTimeout(() => {
+//         return Promise.resolve('this is resolve of promise')
+//     }, 200)
+// }
+// let setTime_ = setTime()
+// setTime_.then(val => {
+//     console.log(val)
+// })
+
+/* -------------------------------------------------------------------------- */
+/*                                promise race                                */
+/* -------------------------------------------------------------------------- */
+
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise1: success')
+    }, 3000)
+})
+let promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject('promise2: failure')
+    }, 1000)
+})
+let promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise3: success')
+    }, 2000)
+})
+
+const fastest = Promise.race([promise1, promise2, promise3])
+
+fastest
+    .then(val => {
+        console.log('this is fastest method', val)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+const allPromise = Promise.all([promise1, promise2, promise3]) // all of them should be resolve
+
+allPromise
+    .then(val => {
+        console.log('this is all promise method', val)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+const allSettleMethod = Promise.allSettled([promise1, promise2, promise3]) // return all of them and doesn't matter if some is fail or some of them success
+
+allSettleMethod
+    .then(val => {
+        console.log('this is allSettled promise method', val)
+    })
+    .catch(err => {
+        console.log(err)
+    })
